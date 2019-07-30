77fab048cef849d7962ec291fd18d8398c9803f7
e <tdp_capricorn@163.com>
2019-07-30 19:04:32 +0800

    a boy can do

--- src/components/Seciton-one.vue
+++ src/components/Seciton-one.vue
@@ -1,13 +1,423 @@
 <template>
+  <div class="m">
+    <div class="head-txt">
+<span>
+<a href="https://   /news/list-57.html">行业动态</a> &nbsp;|&nbsp; <a href="https://   /news/list-113.html">行业新闻</a> &nbsp;|&nbsp; <a href="https://   /news/list-1187.html">植物科普</a> &nbsp;|&nbsp; <a href="https://   /news/list-613.html">植物收藏</a> &nbsp;|&nbsp; <a href="https://   /news/list-1188.html">花木盆景</a> &nbsp;|&nbsp; <a href="https://   /news/list-1171.html">园林景观</a> &nbsp;|&nbsp; <a href="https://   /news/list-579.html">绿色生活</a> &nbsp;|&nbsp; <a href="https://   /news/list-1276.html">绿色家居</a> &nbsp;|&nbsp; <a href="https://   /news/list-110.html">病害防治</a> &nbsp;|&nbsp; <a href="https://   /news/list-289.html">种植养护</a> &nbsp;|&nbsp; <a href="https://   /news/">更多<i>&gt;</i></a>
+</span>
+      <strong>资讯中心</strong>
+    </div>
+    <div class="in-b">
+      <div class="news_l">
+        <div class="picMarquee-top">
+          <div class="bd">
+            <div class="tempWrap" style="overflow:hidden; position:relative; height:561px">
+              <ul class="picList" style="height: 2431px; position: relative; padding: 0px; margin: 0px; top: -1683px;"><li class="clone" style="height: 187px;">
+              <div class="pic"><a href="" target="_blank"><img src="image/ber.jpg" data-n="1" ></a></div>
+              <div class="title"><a href="https://   /news/153944.html" target="_blank">夏天来了，让我们一起做个“吃瓜群众”!</a></div>
+            </li>
+              <li style="height: 187px;">
+                <div class="pic"><a href="https://   /news/154498.html" target="_blank"><img src="image/weather.jpg" data-n="1" ></a></div>
+                <div class="title"><a href="https://   /news/154498.html" target="_blank">简而美的绿色家居，精致的生活需要用心经营！</a></div>
+              </li>
+              <li style="height: 187px;">
+                <div class="pic"><a href="https://   /news/154450.html" target="_blank"><img src=" " data-n="1"     ></a></div>
+                <div class="title"><a href="https://   /news/154450.html" target="_blank">收藏 | 55部植物纪录片带你游遍全球</a></div>
+              </li>
+              <li style="height: 187px;">
+                <div class="pic"><a href="https://   /news/154423.html" target="_blank"><img src=" " data-n="1"     ></a></div>
+                <div class="title"><a href="https://   /news/154423.html" target="_blank">教你认识身边的中草药，有图有真相！【建议收藏】</a></div>
+              </li>
+              <li style="height: 187px;">
+                <div class="pic"><a href="https://   /news/154229.html" target="_blank"><img src=" " data-n="1"     ></a></div>
+                <div class="title"><a href="https://   /news/154229.html" target="_blank">最牛、最全的培养土配制方法！花友必备！</a></div>
+              </li>
+              <li style="height: 187px;">
+                <div class="pic"><a href="https://   /news/154083.html" target="_blank"><img src=" " data-n="1"     ></a></div>
+                <div class="title"><a href="https://   /news/154083.html" target="_blank">别买路边削好的菠萝块 挑菠萝有绝招！</a></div>
+              </li>
+              <li style="height: 187px;">
+                <div class="pic"><a href="https://   /news/154024.html" target="_blank"><img src=" " data-n="1"     ></a></div>
+                <div class="title"><a href="https://   /news/154024.html" target="_blank">生啤、熟啤、原浆、纯生、干啤、冰啤、扎啤有什么区别？</a></div>
+              </li>
+              <li style="height: 187px;">
+                <div class="pic"><a href="https://   /news/143415.html" target="_blank"><img src=" " data-n="1"     ></a></div>
+                <div class="title"><a href="https://   /news/143415.html" target="_blank">每月一朵花，你是哪种花？</a></div>
+              </li>
+              <li style="height: 187px;">
+                <div class="pic"><a href="https://   /news/153986.html" target="_blank"><img src=" " data-n="1"     ></a></div>
+                <div class="title"><a href="https://   /news/153986.html" target="_blank">被人遗忘的史前巨人，植物界的熊猫级树种——水杉</a></div>
+              </li>
+              <li style="height: 187px;">
+                <div class="pic"><a href="https://   /news/153944.html" target="_blank"><img src=" " data-n="1"     ></a></div>
+                <div class="title"><a href="https://   /news/153944.html" target="_blank">夏天来了，让我们一起做个“吃瓜群众”!</a></div>
+              </li>
+              <li class="clone" style="height: 187px;">
+                <div class="pic"><a href="https://   /news/154498.html" target="_blank"><img src=" " data-n="1"     ></a></div>
+                <div class="title"><a href="https://   /news/154498.html" target="_blank">简而美的绿色家居，精致的生活需要用心经营！</a></div>
+              </li><li class="clone" style="height: 187px;">
+                <div class="pic"><a href="https://   /news/154450.html" target="_blank"><img src=" " data-n="1"     ></a></div>
+                <div class="title"><a href="https://   /news/154450.html" target="_blank">收藏 | 55部植物纪录片带你游遍全球</a></div>
+              </li><li class="clone" style="height: 187px;">
+                <div class="pic"><a href="https://   /news/154423.html" target="_blank"><img src=" " data-n="1"     ></a></div>
+                <div class="title"><a href="https://   /news/154423.html" target="_blank">教你认识身边的中草药，有图有真相！【建议收藏】</a></div>
+              </li></ul></div>
 
+          </div>
+        </div>
+
+      </div>
+      <div class="news_m">
+        <h2><a href="https://   /news/154678.html" target="_blank" title="中国花协通报国花网上民意调查情况：牡丹胜出！"><span>中国花协通报国花网上民意调查情况：牡丹胜出！</span></a></h2><div class="list-txt ">
+        <ul>
+          <li><span class="f_r">&nbsp;07-26</span><a href="https://   /news/154698.html" target="_blank" title="烈日炎炎大暑后，别忘了多吃这些食材来解暑~">烈日炎炎大暑后，别忘了多吃这些食材来解暑~</a></li>
+          <li><span class="f_r">&nbsp;07-26</span><a href="https://   /news/154695.html" target="_blank" title="对高温敏感的金钱木，原来夏天也是开花的呀!">对高温敏感的金钱木，原来夏天也是开花的呀!</a></li>
+          <li><span class="f_r">&nbsp;07-25</span><a href="https://   /news/154684.html" target="_blank" title="老菜农绝不外传的买菜技巧，太厉害了！">老菜农绝不外传的买菜技巧，太厉害了！</a></li>
+          <li><span class="f_r">&nbsp;07-25</span><a href="https://   /news/154690.html" target="_blank" title="作为水果，葡萄竟比阿司匹林更能阻止血栓形成!">作为水果，葡萄竟比阿司匹林更能阻止血栓形成!</a></li>
+          <li><span class="f_r">&nbsp;07-25</span><a href="https://   /news/154686.html" target="_blank" title="万物皆可藤椒，小小藤椒，竟有如此魔力!">万物皆可藤椒，小小藤椒，竟有如此魔力!</a></li>
+          <li><span class="f_r">&nbsp;07-24</span><a href="https://   /news/154683.html" target="_blank" title="不知道跑步后吃什么好？那就对号入座吧！">不知道跑步后吃什么好？那就对号入座吧！</a></li>
+          <li><span class="f_r">&nbsp;07-24</span><a href="https://   /news/154682.html" target="_blank" title="对于刚入手的盆景，这样“盘一下”才更美！">对于刚入手的盆景，这样“盘一下”才更美！</a></li>
+          <li><span class="f_r">&nbsp;07-24</span><a href="https://   /news/154681.html" target="_blank" title="这样管理多肉红宝石，还怕出不了状态吗？">这样管理多肉红宝石，还怕出不了状态吗？</a></li>
+        </ul>
+      </div>
+        <div class="b20 bd-t"></div>
+        <div class="list-txt">
+          <ul>
+            <li><span class="f_r">&nbsp;07-24</span><a href="https://   /news/154680.html" target="_blank" title="家中养花来点豆渣，想不爆盆都难！">家中养花来点豆渣，想不爆盆都难！</a></li>
+            <li><span class="f_r">&nbsp;07-23</span><a href="https://   /news/154678.html" target="_blank" title="中国花协通报国花网上民意调查情况：牡丹胜出！">中国花协通报国花网上民意调查情况：牡丹胜出！</a></li>
+            <li><span class="f_r">&nbsp;07-23</span><a href="https://   /news/154676.html" target="_blank" title="都说多吃苋菜能补血！那夏季要多吃吗？">都说多吃苋菜能补血！那夏季要多吃吗？</a></li>
+            <li><span class="f_r">&nbsp;07-23</span><a href="https://   /news/154675.html" target="_blank" title="夏季养花，最常见的问题及解决方法，提前收藏~">夏季养花，最常见的问题及解决方法，提前收藏~</a></li>
+            <li><span class="f_r">&nbsp;07-22</span><a href="https://   /news/154673.html" target="_blank" title="吃藏红花前，需要先了解的小知识！">吃藏红花前，需要先了解的小知识！</a></li>
+            <li><span class="f_r">&nbsp;07-22</span><a href="https://   /news/154672.html" target="_blank" title="大暑喝茶有讲究，只有选对了才避暑！">大暑喝茶有讲究，只有选对了才避暑！</a></li>
+            <li><span class="f_r">&nbsp;07-19</span><a href="https://   /news/154642.html" target="_blank" title="为夏天而生的向日葵，你知道它需要什么吗?">为夏天而生的向日葵，你知道它需要什么吗?</a></li>
+            <li><span class="f_r">&nbsp;07-19</span><a href="https://   /news/154640.html" target="_blank" title="你说连理情深，一生缱绻。我说玉兰花开，而一往情深~">你说连理情深，一生缱绻。我说玉兰花开，而一往情深~</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="in-r">
+        <div class="head-txt news_mb_3">
+          <span><a href="https://   /news/" target="_blank">更多<i>&gt;</i></a></span><strong>最新</strong>
+        </div>
+        <div class="list-txt ">
+          <ul>
+            <li>
+              <span class="f_r">&nbsp;17小时前</span>
+              <a href="https://   /news/154725.html" target="_blank" title="夏天喝紫菜豆腐汤，补充大量矿物质">夏天喝紫菜豆腐汤，补充大量矿物质</a>
+            </li>
+            <li>
+              <span class="f_r">&nbsp;17小时前</span>
+              <a href="https://   /news/154724.html" target="_blank" title="“三伏天”关于吃的那些事">“三伏天”关于吃的那些事</a>
+            </li>
+            <li>
+              <span class="f_r">&nbsp;20小时前</span>
+              <a href="https://   /news/154723.html" target="_blank" title="定远县西卅店镇建成区绿化养护项目招标公告">定远县西卅店镇建成区绿化养护项目招标公告</a>
+            </li>
+            <li>
+              <span class="f_r">&nbsp;20小时前</span>
+              <a href="https://   /news/154722.html" target="_blank" title="市政协院内绿化提升项目磋商公告">市政协院内绿化提升项目磋商公告</a>
+            </li>
+            <li>
+              <span class="f_r">&nbsp;20小时前</span>
+              <a href="https://   /news/154721.html" target="_blank" title="原阳县海绵公园景观绿化工程招标公告">原阳县海绵公园景观绿化工程招标公告</a>
+            </li>
+            <li>
+              <span class="f_r">&nbsp;20小时前</span>
+              <a href="https://   /news/154720.html" target="_blank" title="那坡县2018年跨界河流农村生活污水治理工程（平孟镇）施工招标公告">那坡县2018年跨界河流农村生活污水治理工程（平孟镇）施工招标公告</a>
+            </li>
+            <li>
+              <span class="f_r">&nbsp;20小时前</span>
+              <a href="https://   /news/154719.html" target="_blank" title="波密县茶叶新品种引种与示范种植项目苗木及设备采购项目公开招标公告">波密县茶叶新品种引种与示范种植项目苗木及设备采购项目公开招标公告</a>
+            </li>
+            <li>
+              <span class="f_r">&nbsp;20小时前</span>
+              <a href="https://   /news/154718.html" target="_blank" title="东莞市横沥镇公用事业服务中心东莞市横沥镇彩色林带苗木采购及养护项目公开招标公告">东莞市横沥镇公用事业服务中心东莞市横沥镇彩色林带苗木采购及养护项目公开招标公告</a>
+            </li>
+          </ul>
+        </div>
+        <div class="b20 bd-t"></div>
+        <div class="list-txt">
+          <ul>
+            <li>
+              <span class="f_r">&nbsp;21小时前</span>
+              <a href="https://   /news/154717.html" target="_blank" title="东平县城市水系治理工程（银山街-九鑫）公开招标公告">东平县城市水系治理工程（银山街-九鑫）公开招标公告</a>
+            </li>
+            <li>
+              <span class="f_r">&nbsp;21小时前</span>
+              <a href="https://   /news/154716.html" target="_blank" title="2019-2020年城市主题花景苗木采购公开招标公告">2019-2020年城市主题花景苗木采购公开招标公告</a>
+            </li>
+            <li>
+              <span class="f_r">&nbsp;07-28</span>
+              <a href="https://   /news/154715.html" target="_blank" title="湛江市三岭山森林公园2019年热带季雨林造林项目公开招标公告">湛江市三岭山森林公园2019年热带季雨林造林项目公开招标公告</a>
+            </li>
+            <li>
+              <span class="f_r">&nbsp;07-28</span>
+              <a href="https://   /news/154714.html" target="_blank" title="阳春市林业局阳春市2019年省级碳汇林造林项目公开招标公告">阳春市林业局阳春市2019年省级碳汇林造林项目公开招标公告</a>
+            </li>
+            <li>
+              <span class="f_r">&nbsp;07-27</span>
+              <a href="https://   /news/154713.html" target="_blank" title="这样扦插，能让昙花多次爆盆~">这样扦插，能让昙花多次爆盆~</a>
+            </li>
+            <li>
+              <span class="f_r">&nbsp;07-27</span>
+              <a href="https://   /news/154712.html" target="_blank" title="铁树历史再悠久，也会遇到“黄”的时候">铁树历史再悠久，也会遇到“黄”的时候</a>
+            </li>
+            <li>
+              <span class="f_r">&nbsp;07-27</span>
+              <a href="https://   /news/154711.html" target="_blank" title="县城区域园林绿化养护托管采购采购公告">县城区域园林绿化养护托管采购采购公告</a>
+            </li>
+            <li>
+              <span class="f_r">&nbsp;07-27</span>
+              <a href="https://   /news/154710.html" target="_blank" title="曲靖师范学院校园绿化养护服务采购项目 招标公告">曲靖师范学院校园绿化养护服务采购项目 招标公告</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="c_b"></div>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
-    name: 'Seciton-left'
+    name: 'Seciton-one'
   }
 </script>
 
-<style scoped>
+<style  >
+  .m {
+    margin: auto;
+    width: 1200px;
+    background: #fff;
+    clear: both
+  }
+  body {
+    margin: 0;
+    font-size: 12px;
+    color: #333;
+    background: #fff;
+  }
+  * {
+    word-break: break-all;
+    font-family: microsoft yahei,Tahoma,Verdana,Arial;
+    -webkit-text-size-adjust: none;
+  }
+  .head-txt {
+    padding: 10px 6px;
+    line-height: 28px;
+    border-bottom: #ddd 1px solid;
+    margin-bottom: 20px;
+    clear: both;
+  }
+  .head-txt span {
+    float: right;
+    font-size: 14px;
+    color: #999;
+  }
+  a:link, a:visited, a:active {
+    color: #333;
+    text-decoration: none;
+  }
+  .head-txt strong {
+    font-size: 18px;
+  }
+  .in-b {
+  }
+  .news_l {
+    width: 230px;
+    float: left;
+  }
+  .news_mb_3 {
+    margin-bottom: 3px
+  }
+
+  .picMarquee-top {
+    margin: 10px auto 0;
+    width: 230px;
+    overflow: hidden;
+    position: relative
+  }
+
+  .picMarquee-top .bd {
+    padding: 10px;
+    border: 1px #ccc solid
+  }
+
+  .picMarquee-top .bd ul {
+    overflow: hidden;
+    zoom:1}
+
+  .picMarquee-top .bd ul li {
+    text-align: center;
+    zoom:1}
+
+  .picMarquee-top .bd ul li .pic {
+    text-align: center
+  }
+
+  .picMarquee-top .bd ul li .pic img {
+    width: 210px;
+    height: 140px;
+    display: block
+  }
+
+  .picMarquee-top .bd ul li .pic a:hover img {
+    border-color: #999
+  }
+
+  .picMarquee-top .bd ul li .title {
+    line-height: 24px;
+    font-size: 12px
+  }
+
+  .picMarquee-supply,.picMarquee-buy {
+    margin: 0 auto;
+    width: 100%;
+    position: relative;
+    border: 1px #e5e5e5 solid;
+    margin-bottom: 20px
+  }
+
+  .picMarquee-supply .hd,.picMarquee-buy .hd {
+    height: 50px;
+    line-height: 50px;
+    padding: 0 10px;
+    text-align: center;
+    position: relative
+  }
+
+  .picMarquee-buy .hd h2 {
+  /*background: url(images/index/qiugou.png) no-repeat;*/
+    background-size: 200%;
+    position: absolute;
+    left: 50%;
+    top: 0;
+    width: 200px;
+    height: 40px;
+    color: #fff;
+    line-height: 28px;
+    margin-left: -100px;
+    margin-top: -5px
+  }
+
+  .picMarquee-supply .hd h2 {
+  /*background: url(images/index/qiugou.png) no-repeat 0 -42px;*/
+    background-size: 200%;
+    position: absolute;
+    left: 50%;
+    top: 0;
+    width: 200px;
+    height: 40px;
+    color: #fff;
+    line-height: 28px;
+    margin-left: -100px;
+    margin-top: -5px
+  }
+
+  .picMarquee-supply .bd,.picMarquee-buy .bd {
+    padding: 10px;
+    border: 0;
+    border-top: 1px #e5e5e5 solid
+  }
+
+  .picMarquee-supply .bd ul,.picMarquee-buy .bd ul {
+    overflow: hidden;
+    zoom:1}
+
+  .picMarquee-supply .bd ul li,.picMarquee-buy .bd ul li {
+    text-align: center;
+    overflow: hidden;
+    zoom:1}
+
+  .picMarquee-supply .bd .supply-th th {
+    padding: 10px 0;
+    background-color: #fafafa
+  }
+
+  .picMarquee-supply .bd .supply-td td {
+    padding: 5px 0
+  }
+
+  .picMarquee-supply .bd .supply-td td a.s-title {
+    font-size: 13px
+  }
+
+  .picMarquee-supply .bd .supply-td td a.lianxi {
+    border: 1px solid #ff761c;
+    padding: 5px 10px;
+    border-radius: 2px;
+    color: #ff761c
+  }
+
+  .picMarquee-supply .bd .supply-td td a.lianxi:hover {
+    background-color: #ff761c;
+    color: #fff
+  }
+
+  .picMarquee-buy .bd ul li>div {
+    zoom:1;float: left;
+    padding: 6px
+  }
+  .news_m {
+    width: 472px;
+    float: left;
+    margin: 0 15px
+  }
+  .news_m {
+    width: 472px;
+    float: left;
+    margin: 0 15px
+  }
+  .b20 {
+    height: 20px
+  }
+  .picMarquee-buy .bd .buy-t {
+    background-color: #fafafa;
+    width: 164px;
+    padding: 5px 10px;
+    font-size: 16px
+  }
+
+  .picMarquee-buy .bd .buy-t p {
+    margin: 0
+  }
+
+  .picMarquee-buy .bd .buy-m {
+    padding: 5px 20px;
+    border-bottom: 1px solid #f3f3f3
+  }
+
+  .picMarquee-buy .bd .buy-b {
+    padding: 20px 0;
+    text-align: center
+  }
+
+  .picMarquee-buy .bd .buy-b a {
+    border: 1px solid red;
+    padding: 5px 10px;
+    border-radius: 5px;
+    color: red
+  }
+  .list-txt li {
+    height: 32px;
+    line-height: 32px;
+    overflow: hidden;
+    font-size: 14px;
+  /*background: url(image/li_dot.gif) no-repeat 6px center;*/
+    padding: 0 16px
+  }
+
+  .picMarquee-buy .bd .buy-b a:hover {
+    background-color: #ee2e2c;
+    color: #fff
+  }
+  .bd-t {
+    border-top: #ddd 1px solid
+  }
+  .in-r {
+    width: 468px;
+    float: left
+  }
+  .c_b{
+    clear: both;
+  }
 
 </style>
